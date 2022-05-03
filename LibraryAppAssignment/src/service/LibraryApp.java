d1d877545aea328bbd0b0105519f2b987475c3c5
ReallyBigTree <TreeAnon@protonmail.com>
2022-05-02 21:57:46 -0400

    From LibraryApp, extracted methods related to checking input into a new class InputChecker to remediate blob/spaghetti code

--- LibraryAppAssignment/src/service/LibraryApp.java
+++ LibraryAppAssignment/src/service/LibraryApp.java
@@ -171,44 +171,7 @@ public class LibraryApp {
 	}
 	
 	
-	public static boolean isDecimalNumber(String stringNumber) {
-		String hasSpecial = ":";
-		String regexDecimalPattern = "[+-]?([0-9]*[.])?[0-9]+";
-		Pattern pattern = Pattern.compile(regexDecimalPattern);
-		Matcher matcher = pattern.matcher(stringNumber);
-		
-		if(matcher.matches()) {	
-			return true;
-			
-		}
-		else if (stringNumber.contains(hasSpecial)) {
-			return false;
-		}
-		else {
-			return false;
-		}
-	}
-	public static boolean isIntegerNumber(String stringNumber) {
-		String rgx = "^[0-9]*$";
-		Pattern pattern = Pattern.compile(rgx);
-		Matcher matcher = pattern.matcher(stringNumber);
-		if(matcher.matches()) {
-			return true;
-		}else {
-			return false;
-		}
-	}
-	public static boolean isEnglishKeyboard(String string) {
-		//This is not working as intended, and the sublist contains titles and authors in other languages
-		String rgx = "[a-zA-Z0-9\\t\\n ./<>?;:\"'`!@#$%^&*()\\[\\]{}_+=|\\\\-]";
-		Pattern pattern = Pattern.compile(rgx);
-		Matcher matcher = pattern.matcher(string);
-		if(matcher.matches()) {
-			return true;
-		}else {
-			return false;
-		}
-	}
+
 	
 	
 	
